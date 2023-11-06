86aa08076af8b43d79897e522dbe1f1598250965
alyssacamille <143060589+alyssacamille@users.noreply.github.com>
2023-11-06 06:33:46 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# saki-portfolio-ver-1.5
+# porfolio-ver-1.5
+# still under development
+# updates: added hexagon thingy (has to redirect somewhere tho)
+# better margin/ padding 
+# added collapsable inside about me toggle
+
+# future me pls debug this properly 
---
+++ icon.js
@@ -0,0 +1,53 @@
+// function changeImage()
+// {
+  
+//   var img = document.getElementById("image");
+//   img.src = "saki icon.png" ;
+
+// }
+// function changeImage ()
+// {
+
+// <link href="darkmode.css" rel="stylesheet" type="text/css"></link>
+// }
+// // }
+
+// @import "darkmode.css"
+
+// side nar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
+
+// function changeImage (image){
+//   if(document.getElementById(image).className=="saki icon.png")
+//   {
+//     document.getElementById(image).className = "lychi icon.png";
+//   }else{
+//     document.getElementById(image).className = "saki icon.png";
+//   }
+// }
+
+
+
+// if(img.src.match(/blank/)) img.src = "black.jpg";
+// else img.src = "blank.jpg";
+
+// if(img.src.match("saki icon.png")) img.src = "saki icon.png";
+// else img.src = "ly icon.png";
+
+
+
+
+// ito ang footer
+// ///////////////////////////////////////////////////
+
